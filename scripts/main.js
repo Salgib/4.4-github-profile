c3e27b8dba50e3bb7d8cbf4a90294edce4b21be3
Shawn Gibson <saljgib@gmail.com>
2015-05-28 13:52:34 -0400

    added token.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,15 @@
   $(document).ready(function(){
     $('body').prepend(JST['application']());
   });
+
+  $(document).ready(function(){
+   $.ajax({
+     url: "https://api.github.com/user",
+     headers: {
+       "Authorization": "token " + GITHUB_TOKEN
+     }
+   }).then(function(user) {
+     console.log(user);
+   });
+
 })();
