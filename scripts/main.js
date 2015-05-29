3c9f21c0d14cc5111ba381586dfb92c4a50fc143
Shawn Gibson <saljgib@gmail.com>
2015-05-28 20:15:48 -0400

    implemented token for authentication

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,49 @@
   'use strict';
 
   $(document).ready(function(){
-    $('body').prepend(JST['application']());
+    $.ajax({
+      url: "https://api.github.com/user",
+      headers: {
+        "Authorization": "token " + GITHUB_TOKEN
+      }
+    }).then(function(user) {
+      console.log(user);
+    });
+
+    $.ajax({
+      url: "https://api.github.com/users/salgib/repos",
+      headers: {
+        "Authorization": "token " + GITHUB_TOKEN
+      }
+    }).then(function(user) {
+      console.log(user);
+    });
+
   });
 
-  $(document).ready(function(){
-   $.ajax({
-     url: "https://api.github.com/user",
-     headers: {
-       "Authorization": "token " + GITHUB_TOKEN
-     }
-   }).then(function(user) {
-     console.log(user);
-   });
 
+
+
+
+
+
+
+
+
+
+
+
+
+  // $(document).ready(function(e){
+  //   var code = window.location.href.match(/\?code=(.*)/)[1];
+  //   if(code) {
+  //     $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+  //      console.log(data);
+  //     });
+  //   }
+  // });
+  //
+  // $('button').on('click', function(e){
+  //   window.location.replace('https://github.com/login/oauth/authorize?client_id=30b3ed64b34eadf42a0a');
+  // });
 })();
